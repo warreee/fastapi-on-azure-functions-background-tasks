ad6b8924a82ea15e402d1091e1534afddebc2c7a
Ward Schodts <schodtsward@gmail.com>
2024-05-06 20:21:10 +0200

    Example with synchronous file writing

--- WrapperFunction/__init__.py
+++ WrapperFunction/__init__.py
@@ -8,6 +8,26 @@ from fastapi import BackgroundTasks
 app = fastapi.FastAPI()
 
 
+def write_notification():
+    print("Writing to file")
+    with open("log.txt", mode="w") as email_file:
+        # loop over 1 to 10000 and write to file
+        content = ""
+        for i in range(1, 100000000):
+            # write to file
+            content += f"notification for {i}: some message here\n"
+        email_file.write(content)
+    print("Writing to file completed")
+
+
+@app.get("/send-notification")
+def send_notification(background_tasks: BackgroundTasks):
+    print('Going to send notifications in the background')
+    background_tasks.add_task(write_notification)
+    print("Notification sent in the background")
+    return {"message": "Notifications sent in the background"}
+
+
 async def background_task():
     logging.info("Background task started")
     await asyncio.sleep(5)
