7fa5e4ead3b79d64862c3a3162462d5e61c9d8e3
Ward Schodts <schodtsward@gmail.com>
2024-05-06 09:20:33 +0200

    Background task example

--- WrapperFunction/__init__.py
+++ WrapperFunction/__init__.py
@@ -1,8 +1,24 @@
-import azure.functions as func
+import logging
 
 import fastapi
+import asyncio
+
+from fastapi import BackgroundTasks
 
 app = fastapi.FastAPI()
+
+
+async def background_task():
+    logging.info("Background task started")
+    await asyncio.sleep(5)
+    logging.info("Background task finished")
+
+
+@app.get("/do_background_task")
+async def do_background_task(background_tasks: BackgroundTasks):
+    background_tasks.add_task(background_task)
+    return {"message": "Background task started, this response should return immediately."}
+
 
 @app.get("/sample")
 async def index():
